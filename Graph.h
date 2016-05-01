a385bbb4ddc8313a0e239d92f7a9c084b05172cc
Louis <louisremus@gmail.com>
2016-05-01 14:52:13 +0200

    Update Graph.h
    Définition des fonctions de base

--- Graph.h
+++ Graph.h
@@ -2,3 +2,26 @@
 Auteurs : Charles Maussion et Louis Rémus.
 PI INF 442 : Shortest Paths.
 */
+#ifndef GRAPH
+#define GRAPH
+
+// STL includes
+#include <iostream>
+#include <vector>
+
+template <typename TypeVertice, typename TypeArc>
+class Graph {
+  public :
+    Graph();
+    ~Graph();
+
+    // Deux méthodes d'ajout d'un Arc.
+    TypeArc * ajouterArc(TypeArc * arc);
+    TypeArc * ajouterArc(TypeVertice * V1, TypeVertice * V2 );
+
+    // Méthode d'ajout d'un Vertice.
+    TypeVertice * ajouterVertice(TypeVertice * vertice);
+
+};
+
+#endif
