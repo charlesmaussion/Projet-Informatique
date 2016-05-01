f7d30a969c92300e295256cc0baad1b96af83ed3
Louis <louisremus@gmail.com>
2016-05-01 15:11:43 +0200

    Update Graph.h
    Ajout de nouvelles méthodes.

--- Graph.h
+++ Graph.h
@@ -8,6 +8,9 @@ PI INF 442 : Shortest Paths.
 // STL includes
 #include <iostream>
 #include <vector>
+#include <list>
+
+using namespace std;
 
 template <typename TypeVertice, typename TypeArc>
 class Graph {
@@ -15,12 +18,35 @@ class Graph {
     Graph();
     ~Graph();
 
-    // Deux méthodes d'ajout d'un Arc.
-    TypeArc * ajouterArc(TypeArc * arc);
-    TypeArc * ajouterArc(TypeVertice * V1, TypeVertice * V2 );
+    // Retourne le nombre de Vertices du graphe.
+    int size() const;
 
-    // Méthode d'ajout d'un Vertice.
-    TypeVertice * ajouterVertice(TypeVertice * vertice);
+
+    // Ajout d'un Arc.
+    TypeArc* ajouterArc(TypeArc* arc);
+    TypeArc* ajouterArc(TypeVertice* V1, TypeVertice* V2);
+
+    // Ajout d'un Vertice.
+    TypeVertice* ajouterVertice(TypeVertice* vertice);
+
+    // Récupérer les Arcs du graphe, ou les Arcs d'un Vertice.
+
+    vector<TypeArc*> recupererArcs();
+    vector<TypeArc*> recupererArcs(TypeVertice* vertice);
+
+
+    // Récupérer les successeurs d'un Vertice.
+    list<TypeVertice*> successeurs(TypeVertice* vertice);
+
+    // Reliés?
+    bool sontRelies(TypeVertice* V1, TypeVertice* V2);
+
+    // Retrait d'un Arc.
+    void retirerArc(TypeArc* arc);
+    void retirerArc(TypeVertice* V1, TypeVertice* V2);
+
+    // Retrait d'un Vertice (ce qui retire tous les Arcs qui y sont connectés)
+    void retraitVertice(TypeVertice* vertice);
 
 };
 
